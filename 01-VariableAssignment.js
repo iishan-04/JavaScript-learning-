8bfc15d9d5551564b448c1e359e26117b0d89f22
iishan-04 <ishansisodiya04@gmail.com>
2024-10-25 13:47:14 -0700

    Create hello-world.js

---
+++ 01-VariableAssignment.js
@@ -0,0 +1,35 @@
+// assignment variable
+var FullNmae;
+console.log("FullName: ", FullNmae);
+
+FullNmae = "Ishan";
+console.log("FullName: ", FullNmae);
+
+FullNmae = "Ishan Sisodiya";
+console.log("FullName: ", FullNmae);
+
+var myLove = "I Love Only Js";
+console.log("myLove: ", myLove);
+
+var FirstName = "ishan";
+console.log("FirstName: ", FirstName);
+
+var  LastName = "sisodiya";
+console.log("LastName: ", LastName);
+
+var age = "21";
+console.log("age: ", age);
+
+var address = "JDA Colony Shastri Nager Jabalpur";
+console.log("address: ", address);
+
+var PinCode = "482003";
+console.log("PinCode: ", PinCode);
+
+var Gender = "Male";
+console.log("Gender: ", Gender);
+
+var IsMarried = "no";
+console.log("IsMarried: ", IsMarried);
+
+
